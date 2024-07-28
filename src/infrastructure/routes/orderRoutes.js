fc97ae8be7eb5a6440d1b6980e138a3d54d80805
VSSSP <victorssspaula@live.com>
2024-07-28 19:39:33 -0300

    feat: add order routes

---
+++ src/infrastructure/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+
+router.post('/orders', orderController.createOrder);
+router.get('/orders', orderController.getOrders);
+router.put('/orders/:id/status', orderController.updateOrderStatus);
+
+module.exports = router;
